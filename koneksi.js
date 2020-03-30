ec845442a7cac767d6faaf9c3e6ecfa517680343
zeffanyasyahriel <zeffanyasyahriel244@outlook.com>
2020-03-30 16:26:25 +0700

    membuat file controller

---
+++ koneksi.js
@@ -0,0 +1,14 @@
+var mysql = require('');
+
+// buat koneksi database
+const conn = mysql.createConnection({
+    host:'localhost',user:'root',
+    password:'',
+    database:'dbrestapi';
+});
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('Mysql terkoneksi');
+});
+
+module.exports = conn;
