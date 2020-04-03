a4d7d4c2c67bd3c542cdcc10a57b40f3c04dfec4
zeffanyasyahriel <zeffanyasyahriel244@outlook.com>
2020-04-03 13:46:16 +0700

    berhasil menampilkan semua data mahasiswa

--- koneksi.js
+++ koneksi.js
@@ -1,10 +1,11 @@
-var mysql = require('');
+var mysql = require('mysql');
 
 // buat koneksi database
 const conn = mysql.createConnection({
-    host:'localhost',user:'root',
+    host:'localhost',
+    user:'root',
     password:'',
-    database:'dbrestapi';
+    database:'dbrestapi'
 });
 conn.connect((err)=>{
     if(err) throw err;
